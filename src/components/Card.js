a630fb8e784f7ede6788428172dcb0bfd37c2da4
Dan Lindsey <vc.dan.lindsey@lowes.com>
2020-04-25 21:39:32 -0400

    Rough hands

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 /*
     <Card />
     suit - letter of suit (eg: S)
     face - letter of face (eg: A, K, Q, 10=T)
 */
-export default ({ suit, face, onClick=()=>{}, onMouseOver=()=>{}}) => {
+export default ({ suit, face, disabled, onClick=()=>{}, onMouseOver=()=>{}}) => {
     const cardImg = validSuit(suit) && validFace(face) ? String(suit + face).toUpperCase() : 'BACK';
     return (
-        <CardWrapper onClick={e => onClick(e,{suit,face})} onMouseOver={e => onMouseOver(e,{suit,face})} className="card">
+        <CardWrapper
+            disabled={disabled}
+            onClick={e => onClick(e,{suit,face})} 
+            onMouseOver={e => onMouseOver(e,{suit,face})}
+            className={['card', disabled ? 'disabled' : ''].join(' ')}>
             <img src={ require(`./assets/cards/${cardImg}.png`) } alt={`${face}${suit}`} />
         </CardWrapper>
     );
@@ -55,16 +59,71 @@ const CardWrapper = styled.span`
         max-width: 427px;
         max-height: 600px;
     }
+    ${
+        props => props.disabled && `
+            ::after {
+                content:'';
+                display: block;
+                position: absolute;
+                top: 0;
+                left: 0;
+                z-index: 1;
+                width: 100%;
+                height: 100%;
+                border-radius: 10px;
+                background: rgba(0, 0, 0, .6);
+            }
+        `
+    }
 `;
 
 export const HandWrapper = styled.div`
-    .card { 
+    display: inline-block;
+    width: ${props => props.children.length * 2 + 10}em;
+    height: 18em;
+    position: relative;
+    left: ${props => props.children.length * 0.2}em;
+    bottom: -${props => props.children.length * 0.65}em;
+    .card {
+        position: absolute;
+        cursor: pointer;
         img {
-            max-width: 427px;
-            max-height: 600px;
+            max-width: 10.675rem;
+            max-height: 15rem;
+        }
+        :hover {
+            margin-top: -2.5em;
+            padding-bottom: 2.5em;
+        }
+    }
+    .card.disabled {
+        cursor: default;
+        :hover {
+            margin-top: 0;
+            padding-bottom: 0;
         }
     }
     ${
-        props => console.log(props)
+        props => {
+            let styles = '';
+            const offsetTop = -0.4;
+            const offsetLeft = 2;
+            const middle = props.children.length / 2;
+            for(let i = 0; i < props.children.length; i++) {
+                // middle cards should be highest so get the min distance from either end for offset
+                const verticalOffset = Math.min(i, props.children.length - 1 - i);
+                console.log(`Card #${i+1} of ${props.children.length} verticalOffset ${verticalOffset * -0.4}`)
+                const style = `translate(${Math.abs(i - middle) * 0.25}rem, ${Math.abs(i - middle) * 0.15}rem) rotate(${(i - middle) * 3}deg)`;
+                styles += `.card:nth-of-type(${i+1}) {
+                    top: ${verticalOffset * offsetTop}rem;
+                    left: ${i * offsetLeft}rem;
+                    -moz-transform: ${style};
+                    -webkit-transform: ${style};
+                    -o-transform: ${style};
+                    transform: ${style};
+                }`
+            }
+            return css`${styles}`;
+        }
     }
 `;
--- stories/card.stories.js
+++ stories/card.stories.js
@@ -13,23 +13,13 @@ DefaultCard.story = {
   name: 'Default',
 };
 
-export const AceOfSpades = () => <Card suit="S" face="A" />;
+const onEvent = (e, { suit,face }) => alert(suit+face);
+export const EventProps = () => <Card suit="S" face="A" onClick={onEvent} onMouseOver={onEvent} />;
 
-AceOfSpades.story = {
-  name: 'Ace of Spades',
+EventProps.story = {
+  name: 'Event Props',
 };
 
-export const ClickProp = () => <Card suit="S" face="A" onClick={ (e, { suit,face }) => alert(suit+face) } />;
-
-ClickProp.story = {
-  name: 'Click for CardID',
-};
-
-export const HoverProp = () => <Card suit="S" face="A" onMouseOver={ (e, { suit,face }) => alert(suit+face) } />;
-
-HoverProp.story = {
-  name: 'Hover for CardID',
-};
 
 const CardWrapper = styled.span`
   .card img {
@@ -37,20 +27,66 @@ const CardWrapper = styled.span`
     max-height: 217px;
   }
 `;
-
 export const StyledCard = () => <CardWrapper><Card suit="S" face="A" /></CardWrapper>;
 
 StyledCard.story = {
-  name: 'With Custom Styling',
+  name: 'Custom Styling',
 };
 
-export const HandOfCards = () => (
-  <HandWrapper>
-    <Card suit="S" face="A" />
-    <Card suit="S" face="K" />
-  </HandWrapper>
+export const Hands = () => (
+  <>
+      <HandWrapper>
+        <Card suit="S" face="A" />
+        <Card suit="S" face="K" />
+      </HandWrapper>
+      <HandWrapper>
+        <Card suit="S" face="A" />
+        <Card suit="S" face="K" />
+        <Card suit="S" face="Q" />
+      </HandWrapper>
+      <HandWrapper>
+        <Card suit="S" face="A" />
+        <Card suit="S" face="K" />
+        <Card suit="S" face="Q" />
+        <Card suit="S" face="J" />
+        <Card suit="S" face="T" />
+        <Card suit="S" face="9" />
+      </HandWrapper>
+      <HandWrapper>
+        <Card suit="S" face="A" />
+        <Card suit="S" face="K" />
+        <Card suit="S" face="Q" />
+        <Card suit="S" face="J" />
+        <Card suit="S" face="T" />
+        <Card suit="S" face="9" />
+        <Card suit="S" face="8" />
+        <Card suit="S" face="7" />
+        <Card suit="S" face="6" />
+        <Card suit="S" face="5" />
+        <Card suit="S" face="4" />
+        <Card suit="S" face="3" />
+        <Card suit="S" face="2" />
+      </HandWrapper>
+  </>
 );
 
-HandOfCards.story = {
-  name: 'Hand of Cards',
+Hands.story = {
+  name: 'Grouped Hands',
+};
+
+export const DisabledCards = () => (
+  <>
+      <HandWrapper>
+        <Card suit="S" face="A" />
+        <Card suit="S" face="K" />
+        <Card suit="S" face="Q" />
+        <Card suit="S" face="J" disabled />
+        <Card suit="S" face="T" />
+        <Card suit="S" face="9" />
+      </HandWrapper>
+  </>
+);
+
+DisabledCards.story = {
+  name: 'Disabled Cards',
 };
