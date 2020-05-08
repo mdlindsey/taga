0d86e4558a3c500d488b5506490e860c2fff6cde
Dan Lindsey <dan@mdlindsey.com>
2020-05-08 09:38:56 -0400

    Build

--- build/index.js
+++ build/index.js
@@ -1,5 +1,14 @@
 // ------------ Copy app.yaml to storybook-static ---------------- //
 const fs = require('fs-extra');
+const mv = require('./moveDir');
+
+try {
+  // Throws error but seems to succeed...
+  mv('./storybook-static', './storybook-static/www');
+} catch(e) {
+  console.log(e);
+}
+
 const path = require('path');
 fs.copySync(
     path.resolve(__dirname,'./storybook.app.yaml'), 
